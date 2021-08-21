e58307558648d0a6bee5b2775598640b59745892
Anurag30112003 <anuragsharma011011@gmail.com>
2021-08-21 13:39:38 +0530

    Added template python

---
+++ start.py
@@ -0,0 +1,13 @@
+'''
+Question Link === 
+
+'''
+#solution starts from here
+def fn(n):
+    print()
+n = input()
+# n = list(map(float, input().rstrip().split()))
+# arraySize = int(input()) #array size 
+# arr = [(input()) for i in range(arraySize)] #input given by user
+# print(arr)
+fn(n)    
